2a8ea7b602829131daeb9128cec48a2f7814b696
linwens <lws200841001@gmail.com>
2019-02-20 23:36:12 +0800

    增加上传文章接口，没测

--- controllers/ajaxHandler.js
+++ controllers/ajaxHandler.js
@@ -2,6 +2,8 @@
 //import {Login, Regist} from '../dataBase/users'
 var Login = require('../dataBase/users').Login;
 var Regist = require('../dataBase/users').Regist;
+//---------------------------文章相关
+var Add_article = require('../dataBase/articles').Add_article;
 //注册接口
 var fn_signup = async (ctx, next) => {
     var name = ctx.request.body.name || '',
@@ -30,7 +32,35 @@ var fn_signin = async (ctx, next) => {
         ctx.render('fail.html');
     }
 };
+//文章相关接口-----------------------------------------
+// 新增
+var fn_article_add = async (ctx, next) => {
+    var status = await Add_article(ctx)
+    console.log(status);
+    ctx.response.body = status;
+}
+// 删除
+var fn_article_del = async (ctx, next) => {
+    let id = ctx.params.id;
+}
+// 更新
+var fn_article_update = async (ctx, next) => {
+    let id = ctx.params.id;
+}
+// 获取详情
+var fn_article_get = async (ctx, next) => {
+    let id = ctx.params.id;
+}
+// 获取列表
+var fn_article_list = async (ctx, next) => {
+    let tag = ctx.params.tag;
+}
 module.exports = {
     "POST /signin" : fn_signin,
-    "POST /signup" : fn_signup
+    "POST /signup" : fn_signup,
+    "POST /article/add" : fn_article_add,
+    "POST /article/del/:id" : fn_article_del,
+    "POST /article/update/:id" : fn_article_update,
+    "POST /article/get/:id" : fn_article_get,
+    "POST /article/list/:tag" : fn_article_list,
 }
--- dataBase/users.js
+++ dataBase/users.js
@@ -73,43 +73,6 @@ var Regist = async (ctx) => {
             }
         }
     }
-
-
-
-    //mongoose里model的实例(这里就是小写的users)不存在find方法，用users.find()会报错，所以用User.find()
-    /* User.find({username:users.username})
-    .then(function(data){
-        if(data&&data!=''&&data!='[]'){
-            res.json({
-                res_code:'0',
-                res_msg:'用户已经存在'
-            });
-            return Promise.reject('用户已存在');
-        }else{
-            return users;
-            //return Promise.resolve(users);//两句的效果是一样的，
-            //return的值会由 Promise.resolve(return的返回值); 进行相应的包装处理，因此不管回调函数中会返回一个什么样的值，最终 then 的结果都是返回一个新创建的promise对象。
-        }
-    }).then(function(users){
-        //实例具有save()方法进行存储，所以用users,而不是用构造函数User
-        users.save()
-        .then(function(data){
-            //返回前端一个唯一id
-            res.json({
-                res_code:'1',
-                res_msg:'注册成功',
-                data:{
-                    uid:data._id
-                }
-            })
-        }).catch(function(err){
-            console.log('save--err');
-            console.log(err);
-        })
-    }).catch(function(err){
-        console.log('find--err');
-        console.log(err);
-    }) */
 }
 exports.Login = Login;
 //注册
